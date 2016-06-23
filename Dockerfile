be5a8cca1eba54c9e8f9069f78c9f122c7276e09
deshraj <deshrajdry@gmail.com>
2016-06-23 09:04:00 -0400

    required files added to host on the server

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM ubuntu:14.04
+RUN apt-get update && apt-get install -y python-pip python-opencv && pip install flask && pip install tornado
+RUN pip install -U flask-cors
+ADD app.py /
+ADD cvfy.py /
+ENTRYPOINT python app.py
---
+++ app.py
@@ -0,0 +1,20 @@
+import cvfy
+
+app = cvfy.register('gh:107.170.77.168:60819434:3000:8000')
+
+@cvfy.crossdomain
+@app.listen()
+def concat():
+        
+    ## receiving the data
+    alltext = cvfy.getTextArray()
+    
+    ## processing the data
+    joined_and_uppercase = (alltext[0] + ' ' + alltext[1]).upper()
+    and_then_reversed = joined_and_uppercase[::-1]
+    
+    ## sending back the data
+    cvfy.sendTextArray([joined_and_uppercase, and_then_reversed])
+    return 'OK'
+        
+app.run()
